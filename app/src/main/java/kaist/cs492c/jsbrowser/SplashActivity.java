65925fbc797bcc454d8e7e4b10dafafb1410de2a
Junsung Lim <wns349@kaist.ac.kr>
2015-09-06 23:53:49 +0900

    added handler to go to WebActivity

--- app/src/main/java/kaist/cs492c/jsbrowser/SplashActivity.java
+++ app/src/main/java/kaist/cs492c/jsbrowser/SplashActivity.java
@@ -1,9 +1,13 @@
 package kaist.cs492c.jsbrowser;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +15,16 @@ public class SplashActivity extends Activity {
 
         setContentView(R.layout.activity_splash);
 
-
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the WebActivity. */
+                Intent mainIntent = new Intent(SplashActivity.this, WebActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
